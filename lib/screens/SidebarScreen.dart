026ec828c463da28dab6a06800650a51533cc755
AbdallaSalem998 <abdallah202180155@gmail.com>
2024-05-11 18:38:18 +0300

    switch Fixed just logic to go :)

--- lib/screens/SidebarScreen.dart
+++ lib/screens/SidebarScreen.dart
@@ -14,6 +14,7 @@ class SideBarScreen extends StatefulWidget {
 class _SideBarScreenState extends State<SideBarScreen> {
 
   DatabaseReference ref = FirebaseDatabase.instance.ref("action");
+  bool door = false,window = false,electricity = false,gas = false,alarm = false,water_pump=false;
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +32,7 @@ class _SideBarScreenState extends State<SideBarScreen> {
       );
     }
 
-    bool door = true,window = true,electricity = true,gas = true,alarm = true;
+    // bool door = true,window = true,electricity = true,gas = true,alarm = true;
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -40,13 +41,14 @@ class _SideBarScreenState extends State<SideBarScreen> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          sideBarRow(Icons.door_front_door, "Door", Switch(value: door, onChanged: (bool value){setState(() async {door=value; await ref.update({"door": door,}); });})),
-          sideBarRow(Icons.window, "Window", Switch(value: window, onChanged: (bool value){setState(() {window=value; });})),
-          sideBarRow(Icons.electric_bolt, "Electricity", Switch(value: electricity, onChanged: (bool value){setState(() {electricity=value; });})),
-          sideBarRow(Icons.cloud_done, "Gas", Switch(value: gas, onChanged: (bool value){setState(() {gas=value; });})),
-          sideBarRow(CupertinoIcons.bell_fill, "Fire Alarm", Switch(value: alarm, onChanged: (bool value){setState(() {alarm=value; });})),
-          ElevatedButton(onPressed: () async {door= !door; await ref.update({"door": door,});}, child: Text("door")),
-          ElevatedButton(onPressed: () async {gas= !gas; await ref.update({"gas": gas,});}, child: Text("gas")),
+          sideBarRow(Icons.door_front_door, "Door", Switch(value: door, onChanged: (bool value) async {door=value; await ref.update({"door": door,});   setState(() {});})),
+          sideBarRow(Icons.window, "Window", Switch(value: window, onChanged: (bool value) async {window=value; await ref.update({"window": window,});   setState(() {});})),
+          sideBarRow(Icons.electric_bolt, "Electricity", Switch(value: electricity, onChanged: (bool value) async {electricity=value; await ref.update({"electricity ": electricity,});   setState((){});})),
+          sideBarRow(Icons.cloud_done, "Gas", Switch(value: gas, onChanged: (bool value) async {gas=value; await ref.update({"gas": gas,});   setState((){});})),
+          sideBarRow(CupertinoIcons.bell_fill, "Fire Alarm", Switch(value: alarm, onChanged: (bool value) async {alarm=value; await ref.update({"alarm": alarm,});   setState((){});})),
+          sideBarRow(Icons.water_drop_outlined, "Water pump", Switch(value: water_pump, onChanged: (bool value) async {water_pump=value; await ref.update({"water_pump": water_pump,});   setState((){});})),
+          // ElevatedButton(onPressed: () async {door= !door; await ref.update({"door": door,});}, child: Text("door")),
+          // ElevatedButton(onPressed: () async {gas= !gas; await ref.update({"gas": gas,});}, child: Text("gas")),
           // ,Row(
           //   children: [
           //     Icon(Icons.door_front_door),
